2a55e2a005b4b094d0f71243badee128d87aa80e
zRain <2296342883@qq.com>
2021-09-27 23:28:03 +0800

    更新model/index.js

---
+++ src/model/index.js
@@ -0,0 +1,6 @@
+export * from './modules/address'
+export * from './modules/goods'
+export * from './modules/order'
+export * from './modules/rent'
+export * from './modules/shop'
+export * from './modules/user'
